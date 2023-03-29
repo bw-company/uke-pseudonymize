c78e1895863164fbb70c6437a5f84482b43601ee
Kengo TODA <toda_k@henry.jp>
2023-03-29 15:56:37 +0800

    build: reflect changes to IDEA config file
    Signed-off-by: Kengo TODA <toda_k@henry.jp>

--- app/src/test/kotlin/jp/henry/uke/mask/MaskEngineSpec.kt
+++ app/src/test/kotlin/jp/henry/uke/mask/MaskEngineSpec.kt
@@ -31,47 +31,56 @@ class MaskEngineSpec : DescribeSpec({
         it("computes age and embeds it into the masked name") {
             val today = Clock.fixed(Instant.parse("2020-03-01T09:00:00.00Z"), zoneId)
             val birthDay = LocalDate.of(2010, 2, 1)
+
             MaskingEngine(0, today).maskPatientName("患者", birthDay) shouldContain "10歳"
         }
         describe("誕生日の前日に年齢を加算する") {
             it("誕生日が1日の場合") {
-                val today = Clock.fixed(Instant.parse("2021-01-31T09:00:00.00Z"), zoneId)
-                LocalDate.ofInstant(today.instant(), zoneId) shouldBe LocalDate.of(2021, 1, 31)
-
                 val birthDay = LocalDate.of(2020, 2, 1)
-                MaskingEngine(0, today).maskPatientName("患者", birthDay) shouldContain "（1歳）"
-            }
-            it("誕生日が1日の場合(2月)") {
-                val endOfTheFebEve = Clock.fixed(Instant.parse("2021-02-27T09:00:00.00Z"), zoneId)
-                val endOfTheFeb = Clock.fixed(Instant.parse("2021-02-28T09:00:00.00Z"), zoneId)
-                val birthDay = LocalDate.of(2020, 3, 1)
 
-                MaskingEngine(0, endOfTheFebEve).maskPatientName("患者", birthDay) shouldContain "（0歳）"
-                MaskingEngine(0, endOfTheFeb).maskPatientName("患者", birthDay) shouldContain "（1歳）"
-            }
-            it("誕生日が1日の場合(うるう年)") {
-                val birthDay = LocalDate.of(2019, 3, 1)
-                val endOfTheFebEve = Clock.fixed(Instant.parse("2020-02-28T09:00:00.00Z"), zoneId)
-                val endOfTheFeb = Clock.fixed(Instant.parse("2020-02-29T09:00:00.00Z"), zoneId)
+                val theDayBefore = Clock.fixed(Instant.parse("2021-01-30T09:00:00.00Z"), zoneId)
+                val endOfTheLastMonth = Clock.fixed(Instant.parse("2021-01-31T09:00:00.00Z"), zoneId)
+                val nextBirthDay = Clock.fixed(Instant.parse("2021-02-01T09:00:00.00Z"), zoneId)
 
-                MaskingEngine(0, endOfTheFebEve).maskPatientName("患者", birthDay) shouldContain "（0歳）"
-                MaskingEngine(0, endOfTheFeb).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+                MaskingEngine(0, theDayBefore).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+                // この時点では1歳だが、その月の1日での年齢を表示するため変更なし
+                MaskingEngine(0, endOfTheLastMonth).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+                MaskingEngine(0, nextBirthDay).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+            }
+            it("誕生日が2日の場合") {
+                val birthDay = LocalDate.of(2020, 2, 2)
+
+                val endOfTheLastMonth = Clock.fixed(Instant.parse("2021-01-31T09:00:00.00Z"), zoneId)
+                val beginningOfThisMonth = Clock.fixed(Instant.parse("2021-02-01T09:00:00.00Z"), zoneId)
+                val endOfTheMonth = Clock.fixed(Instant.parse("2021-02-28T09:00:00.00Z"), zoneId)
+                val beginningOfTheNextMonth = Clock.fixed(Instant.parse("2021-03-01T09:00:00.00Z"), zoneId)
+
+                MaskingEngine(0, endOfTheLastMonth).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+
+                // この時点で1歳となり、その月の1日での年齢を表示するため1際と表示
+                MaskingEngine(0, beginningOfThisMonth).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+
+                MaskingEngine(0, endOfTheMonth).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+                MaskingEngine(0, beginningOfTheNextMonth).maskPatientName("患者", birthDay) shouldContain "（1歳）"
             }
             it("誕生日がそれ以外の場合") {
-                val birthDay = LocalDate.of(2019, 3, 3)
-                val twoDaysBefore = Clock.fixed(Instant.parse("2020-03-01T09:00:00.00Z"), zoneId)
-                val oneDayBefore = Clock.fixed(Instant.parse("2020-03-02T09:00:00.00Z"), zoneId)
-                val endOfTheMonth = Clock.fixed(Instant.parse("2020-03-31T09:00:00.00Z"), zoneId)
-                val beginningOfTheNextMonth = Clock.fixed(Instant.parse("2020-04-01T09:00:00.00Z"), zoneId)
+                checkAll(Arb.int(3, 31)) {
+                    val birthDay = LocalDate.of(2019, 3, it)
 
-                MaskingEngine(0, twoDaysBefore).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+                    val twoDaysBefore = Clock.fixed(Instant.parse("2020-03-${"%02d".format(it - 2)}T09:00:00.00Z"), zoneId)
+                    val oneDayBefore = Clock.fixed(Instant.parse("2020-03-${"%02d".format(it - 1)}T09:00:00.00Z"), zoneId)
+                    val endOfTheMonth = Clock.fixed(Instant.parse("2020-03-31T09:00:00.00Z"), zoneId)
+                    val beginningOfTheNextMonth = Clock.fixed(Instant.parse("2020-04-01T09:00:00.00Z"), zoneId)
 
-                // その月の1日での年齢を表示するため変更なし
-                MaskingEngine(0, oneDayBefore).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+                    MaskingEngine(0, twoDaysBefore).maskPatientName("患者", birthDay) shouldContain "（0歳）"
 
-                // 次の月から年齢が加算される
-                MaskingEngine(0, endOfTheMonth).maskPatientName("患者", birthDay) shouldContain "（0歳）"
-                MaskingEngine(0, beginningOfTheNextMonth).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+                    // その月の1日での年齢を表示するため、誕生日前日でも変更なし
+                    MaskingEngine(0, oneDayBefore).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+
+                    // 次の月から年齢が加算される
+                    MaskingEngine(0, endOfTheMonth).maskPatientName("患者", birthDay) shouldContain "（0歳）"
+                    MaskingEngine(0, beginningOfTheNextMonth).maskPatientName("患者", birthDay) shouldContain "（1歳）"
+                }
             }
         }
     }
