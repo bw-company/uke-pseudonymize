e99c40d15d9d6911c573649ffbd6c3ed7be42063
Kengo TODA <toda_k@henry.jp>
2023-05-10 15:41:12 +0800

    chore: add a missing slash in the resource name
    Signed-off-by: Kengo TODA <toda_k@henry.jp>

--- app/src/main/kotlin/jp/henry/uke/mask/PropertiesVersionProvider.kt
+++ app/src/main/kotlin/jp/henry/uke/mask/PropertiesVersionProvider.kt
@@ -5,7 +5,7 @@ import java.util.Properties
 
 class PropertiesVersionProvider : CommandLine.IVersionProvider {
     override fun getVersion(): Array<String> =
-        this.javaClass.getResourceAsStream("metadata.properties").use {
+        this.javaClass.getResourceAsStream("/metadata.properties").use {
             val prop = Properties()
             prop.load(it)
             arrayOf(prop.getProperty("version") ?: "SNAPSHOT")
